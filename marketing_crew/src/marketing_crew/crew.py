1b27b7b7ffa88c92ed3cb3ff0fc39cd2bbe5f195
Sagar D. <69472208+Sagar3195@users.noreply.github.com>
2025-12-29 11:45:18 +0530

    File Added

---
+++ marketing_crew/src/marketing_crew/crew.py
@@ -0,0 +1,70 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from crewai_tools import SerperDevTool
+
+
+@CrewBase
+class MarketingCrew():
+    """MarketingCrew crew"""
+
+    agents_config = "config/agents.yaml"
+    tasks_config = "config/tasks.yaml"
+
+    @agent
+    def research_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['research_agent'], 
+            tools = [SerperDevTool()],
+            verbose=True
+        )
+
+    @agent
+    def writer_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['writer_agent'], 
+            verbose=True
+        )
+
+    @agent
+    def reviewer_agent(self) -> Agent:
+        return Agent(
+            config= self.agents_config["rewiewer_agent"],
+            verbose = True
+        )
+    
+    @task
+    def research_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['research_task'], 
+            agents=[self.research_agent],
+            tools=[SerperDevTool()],
+            verbose = True
+        )
+
+    @task
+    def writing_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['writing_task'],
+            agents= [self.writer_agent],
+            output_file='output.md',
+            verbose = True
+        )
+
+    @task
+    def reviewing_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['reviewing_task'],
+            agents=[self.reviewer_agent],
+            output_file='improvements.md',
+            verbose=True
+        )
+    @crew
+    def crew(self) -> Crew:	
+        return Crew(
+			agents=self.agents,
+			tasks=self.tasks,
+   			process=Process.sequential
+			
+		)
+    
+    
